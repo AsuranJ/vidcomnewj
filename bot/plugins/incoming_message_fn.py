b240e9e5b6b70ac6c2edca601a51f005b849bfac
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-02-09 08:58:23 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -60,7 +60,7 @@ async def incoming_start_message_f(bot, update):
             if user.status == "kicked":
                await bot.send_message(
                    chat_id=update.chat.id,
-                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/serialcoing).",
                    parse_mode="markdown",
                    disable_web_page_preview=True
                )
@@ -82,7 +82,7 @@ async def incoming_start_message_f(bot, update):
         except Exception:
             await bot.send_message(
                 chat_id=update.chat.id,
-                text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+                text="Something went Wrong. Contact my [Support Group](https://t.me/serialcoing).",
                 parse_mode="markdown",
                 disable_web_page_preview=True)
             return
@@ -92,10 +92,10 @@ async def incoming_start_message_f(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Updates Channel', url='https://t.me/Discovery_Updates')
+                    InlineKeyboardButton('Updates Channel', url='https://t.me/SerialCoIn')
                 ],
                 [
-                    InlineKeyboardButton('Support Group', url='https://t.me/linux_repo')
+                    InlineKeyboardButton('Support Group', url='https://t.me/serialcoing')
                 ]
             ]
         ),
@@ -113,7 +113,7 @@ async def incoming_compress_message_f(bot, update):
           if user.status == "kicked":
              await bot.send_message(
                  chat_id=update.chat.id,
-                 text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                 text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/serialcoing).",
                  parse_mode="markdown",
                  disable_web_page_preview=True
              )
@@ -135,7 +135,7 @@ async def incoming_compress_message_f(bot, update):
       except Exception:
           await bot.send_message(
               chat_id=update.chat.id,
-              text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+              text="Something went Wrong. Contact my [Support Group](https://t.me/serialcoing).",
               parse_mode="markdown",
               disable_web_page_preview=True
           )
